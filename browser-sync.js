606b08730f77dfb771ffddcf3529f47f449a0927
William Poncelet <2681407@edu.collegelacite.ca>
2020-11-08 16:35:00 -0500

    BootStrap Working

--- browser-sync.js
+++ browser-sync.js
@@ -1,14 +1,16 @@
 module.exports = {
-    ui: false,
-    files: '.',
-    proxy: `localhost:${ process.env.PORT }`,
-    notify: false,
-    snippetOptions: {
-        rule: {
-            match: /<\/head>/i,
-            fn: function (snippet, match) {
-                return snippet.replace('id=', `nonce="browser-sync" id=`) + match;
-            }
-        }
-    }
-}
+  ui: false,
+  files: ".",
+  proxy: `localhost:${process.env.PORT}`,
+  notify: false,
+  snippetOptions: {
+    rule: {
+      match: /<\/head>/i,
+      fn: function (snippet, match) {
+        return snippet.replace("id=", `nonce="browser-sync" id=`) + match;
+      },
+    },
+  },
+};
+//1st
+//`nonce="browser-sync"
--- server.js
+++ server.js
@@ -25,6 +25,9 @@ app.use(cors());
 app.use(bodyParser.json({ strict: false }));
 app.use(serveStatic("./public"));
 app.use(sse());
+app.use("/js", express.static(__dirname + "/node_modules/bootstrap/dist/js")); // redirect bootstrap JS
+app.use("/js", express.static(__dirname + "/node_modules/jquery/dist")); // redirect JS jQuery
+app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css")); // redirect CSS bootstrap
 
 // Ajouter vos routes ici
 
