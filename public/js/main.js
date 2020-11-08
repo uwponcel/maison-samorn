64be7fb0e36f7c9994b0dfe53e3a756103469316
William Poncelet <2681407@edu.collegelacite.ca>
2020-11-08 17:37:34 -0500

    Modal buttons fix

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,6 @@
-// Get the modal
-let modal = document.getElementById("id01");
-let signUp = document.getElementById("signUpButton");
-// When the user clicks anywhere outside of the modal, close it
-// window.onclick = function (event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// };
-// signUp.onclick = function () {
-//   $("#myModal").modal();
-//   // if (event.target == signUp) {
-//   //   modal.style.display = "block";
-//   // }
-// };
-$("#signUpButton").on("click", () => {
+$("#navSignUpButton").on("click", () => {
   $("#myModal").modal();
 });
-$("#signUpButton").on("click", () => {
-  $("#myModal").modal();
+$("#signupCancelButton").on("click", () => {
+  $("#myModal").modal("hide");
 });
