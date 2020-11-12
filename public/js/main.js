c849a62e8532dc11983805b8b8430fbf170a6f1f
William Poncelet <2681407@edu.collegelacite.ca>
2020-11-11 22:46:15 -0500

    Footer-Fix & Collapse delay fix

--- public/js/main.js
+++ public/js/main.js
@@ -11,3 +11,16 @@ $("#connectInscription").on("click", () => {
   $("#connectionModal").modal("hide");
   $("#signUpModal").modal();
 });
+
+//Collapse delay fix //TODO Do for all the collapses
+$("#collapseAppetizer").on("show.bs.collapse", function () {
+  // do something…
+  $("#collapseAppetizer").addClass("d-flex flex-wrap justify-content-around");
+});
+
+$("#collapseAppetizer").on("hidden.bs.collapse", function () {
+  // do something…
+  $("#collapseAppetizer").removeClass(
+    "d-flex flex-wrap justify-content-around"
+  );
+});
